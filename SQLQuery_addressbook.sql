2132118300980b86c41d51144f8a332151539a9d
Aneesh <katta.aneesh597@gmail.com>
2022-12-23 18:19:27 +0530

    added insert data into table for address book

--- SQLQuery_addressbook.sql
+++ SQLQuery_addressbook.sql
@@ -1,12 +1,19 @@
 --UC2 Creating table 
-create table AddressBookTable
+Create table Address_Book
 (
-firstName varchar (200),
-lastName varchar(200) ,
-address varchar,
-city varchar,
-state varchar ,
-zip int,
-phoneNumber bigint,
-email varchar
-);
+FirstName varchar(255),
+LastName varchar(255),
+Address varchar(255),
+City varchar(255),
+State varchar(255),
+Zip int,
+PhoneNo int,
+Email varchar(255)
+)
+--UC3 Insert new Contact to AddressBook Table--
+insert into Address_Book values('Ravi','K','hno.18-12-220 ameerpet','hyderabad','TS',500001,987653421,'ravi2@gmail.com')
+insert into Address_Book values('Rakesh','V','hno.1-11-220 dilshuknagar','hyderabad','TS',570001,989653421,'rk@gmail.com')
+insert into Address_Book values('Rajender','G','hno.1-1-20 lbnagar','hyderabad','TS',500081,987659421,'rv@gmail.com')
+--To view inserted contact--
+select * from Address_Book
+
